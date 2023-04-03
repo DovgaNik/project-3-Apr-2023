5c38467cddd90fa7230a9e797d3592d6cb2fd0cb
DovgaNik <nikita.dovgan.05@hotmail.com>
2023-04-03 05:01:12 +0300

    Last projject

---
+++ Mortgage Calculator/Mortgage Calculator/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.DomainUpDown1 = New System.Windows.Forms.DomainUpDown()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(14, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(97, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Total amount"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(117, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(137, 27)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Interest rate %"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(117, 45)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(137, 27)
+        Me.NumericUpDown1.TabIndex = 4
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Location = New System.Drawing.Point(117, 78)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(53, 27)
+        Me.NumericUpDown2.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(57, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Length"
+        '
+        'DomainUpDown1
+        '
+        Me.DomainUpDown1.Items.Add("Months")
+        Me.DomainUpDown1.Items.Add("Years")
+        Me.DomainUpDown1.Location = New System.Drawing.Point(176, 78)
+        Me.DomainUpDown1.Name = "DomainUpDown1"
+        Me.DomainUpDown1.Size = New System.Drawing.Size(78, 27)
+        Me.DomainUpDown1.TabIndex = 7
+        Me.DomainUpDown1.Text = "Months"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(117, 126)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(137, 27)
+        Me.TextBox2.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 133)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(107, 20)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Month amount"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(299, 191)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DomainUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Mortgage calculator"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DomainUpDown1 As DomainUpDown
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Mortgage Calculator/Mortgage Calculator/Form1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Security
+
+Public Class Form1
+    Function Calculate()
+        Dim month As Integer
+        Dim rate As Double
+        Dim amount As Double
+        If DomainUpDown1.Text = "Months" Then
+            month = NumericUpDown2.Value
+        Else
+            month = NumericUpDown2.Value * 12 / 100
+        End If
+
+        rate = NumericUpDown1.Value / 5
+
+        If TextBox1.Text.Trim <> Nothing And IsNumeric(TextBox1.Text) Then
+            amount = Convert.ToDouble(TextBox1.Text)
+        End If
+
+        Dim monthlyPayment As Double = amount * (rate * (1 + rate) ^ month) / ((1 + rate) ^ month - 1)
+
+        TextBox2.Text = monthlyPayment
+    End Function
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Calculate()
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        Calculate()
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        Calculate()
+    End Sub
+
+    Private Sub DomainUpDown1_SelectedItemChanged(sender As Object, e As EventArgs) Handles DomainUpDown1.SelectedItemChanged
+        Calculate()
+    End Sub
+End Class
